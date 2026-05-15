c8f3ba7faabf47dbebb2476a3d801eb6fd6df15d
lavanya <lavanyahegde05@gmail.com>
2026-05-15 12:51:17 +0530

    Added GitHub Actions workflow

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 let notes = [];
-
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
-
 app.get("/notes", (req, res) => {
-  res.json(notes);
+res.json(notes);
 });
-
 app.post("/notes", (req, res) => {
-  const note = req.body.note;
-  notes.push(note);
-  res.json({ message: "Note Added" });
+const note = req.body.note;
+notes.push(note);
+res.json({ message: "Note Added" });
 });
-
 app.delete("/notes/:id", (req, res) => {
-  notes.splice(req.params.id, 1);
-  res.json({ message: "Note Deleted" });
+notes.splice(req.params.id, 1);
+res.json({ message: "Note Deleted" });
 });
-
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+app.listen(5000, () => {
+console.log("Server running on port 5000");
 });
